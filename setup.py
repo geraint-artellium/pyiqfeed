52515e5951a61fffaa862b7adeba5eb16be8d8fe
Geraint Harker <geraint@artellium.ai>
2026-08-19 15:55:17 +0200

    Bump version to reflect long time in prod and update to match current IQConnect.exe protocol.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 
 setup(
     name='pyiqfeed',
-    version='0.9',
+    version='1.0',
     description='Handles connections to IQFeed, the market data feed by DTN',
     url='https://github.com/akapur/pyiqfeed',
-    author='Ashwin Kapur',
-    author_email='ashwin.kapur@gmail.com',
+    author='Ashwin Kapur, Geraint Harker',
+    author_email='ashwin.kapur@gmail.com, geraint@artellium.ai',
     license='GPL v2',
     packages=['pyiqfeed'],
     zip_safe=False, install_requires=['numpy'])
